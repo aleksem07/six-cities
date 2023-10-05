fe8bfdda30a4e78f9804680ccdf3a686a8ec4448
aleksem07 <aleksem07@gmail.com>
2023-10-06 00:07:00 +0300

    feat: transfers all pages in pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,28 @@
-import Main from '../../pages/main/main';
+import Main from '../../pages/main-page/main';
+import Login from '../../pages/login-page/login';
 import OfferCountProps from '../../types/offer-count.types';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from './app.types';
+import FavoritesEmpty from '../../pages/favorites-empty-page/favorites-empty';
+import Page404 from '../../pages/page404-page/page404';
 
 function App({offerCount}: OfferCountProps): JSX.Element {
-  return <Main offerCount={offerCount}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}
+          element={<Main offerCount={offerCount} />}
+        />;
+        <Route path={AppRoute.SignIn}
+          element={<Login />}
+        />;
+        <Route path={AppRoute.Favorites}
+          element={<FavoritesEmpty />}
+        />;
+        <Route path={AppRoute.Page404} element={<Page404 />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/app/app.types.tsx
@@ -0,0 +1,7 @@
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id',
+  Page404 = '/page404',
+}
