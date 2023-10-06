11b6d9b882b6e2e32b3cdf613c066777176153d1
aleksem07 <aleksem07@gmail.com>
2023-10-06 20:00:19 +0300

    feat: add footer and logo components

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 
 const Layout = (): JSX.Element => (
   <div className='page page--gray page--main'>
     <Header />
     <Outlet />
+    <Footer />
   </div>
 );
 
---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,9 @@
+import { LogoProps } from './logo.types';
+
+export function Logo ({width, height}: LogoProps): JSX.Element {
+  return (
+    <img className="header__logo" src="img/logo.svg" alt="6 cities logo"
+      width={width} height={height}
+    />
+  );
+}
